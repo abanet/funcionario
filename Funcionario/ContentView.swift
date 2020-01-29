54fe15566c13f3cb195dc6c8a6f8614189f76d45
abanet <abanet@ucm.es>
2020-01-29 09:58:01 +0100

    Initial Commit

---
+++ Funcionario/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Funcionario
+//
+//  Created by Alberto Banet Masa on 29/01/2020.
+//  Copyright © 2020 Alberto Banet Masa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
